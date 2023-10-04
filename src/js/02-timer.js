4dd8061e50388af4f38dbac88c8dfd35c19b5fa8
Misetr <135700846+Misetr@users.noreply.github.com>
2023-10-04 13:15:18 +0300

    fix code timer

---
+++ src/js/02-timer.js
@@ -0,0 +1,82 @@
+import flatpickr from 'flatpickr';
+import '../../node_modules/flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  input: document.querySelector('#datetime-picker'),
+  startBtn: document.querySelector('button[data-start]'),
+  daysEl: document.querySelector('span[data-days]'),
+  hoursEl: document.querySelector('span[data-hours]'),
+  minutesEl: document.querySelector('span[data-minutes]'),
+  secondsEl: document.querySelector('span[data-seconds]'),
+};
+
+refs.startBtn.setAttribute('disabled', '');
+
+const nowTime = new Date();
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] <= nowTime) {
+      // window.alert('Please choose a date in the future');
+      Notify.failure('Please choose a date in the future');
+      return;
+    }
+    refs.startBtn.removeAttribute('disabled');
+    let diff = selectedDates[0] - nowTime;
+    refs.startBtn.addEventListener('click', onStartBtnClick);
+    function onStartBtnClick() {
+      refs.startBtn.setAttribute('disabled', "");
+      const interval = setInterval(() => {
+        diff -= 1000;
+        const diffObj = convertMs(diff);
+        render(diffObj);
+        transform();
+        if (diff <= 1000) clearInterval(interval);
+      }, 1000);
+    }
+  },
+};
+flatpickr(refs.input, options);
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function transform() {
+  refs.daysEl.classList.add('trans');
+  refs.hoursEl.classList.add('trans');
+  refs.minutesEl.classList.add('trans');
+  refs.secondsEl.classList.add('trans');
+  setTimeout(() => {
+    refs.daysEl.classList.remove('trans');
+    refs.hoursEl.classList.remove('trans');
+    refs.minutesEl.classList.remove('trans');
+    refs.secondsEl.classList.remove('trans');
+  }, 500);
+}
+
+function render({days, hours, minutes, seconds}) {
+  refs.daysEl.textContent = days.toString().padStart(2, '0');
+  refs.hoursEl.textContent = hours.toString().padStart(2, '0');
+  refs.minutesEl.textContent = minutes.toString().padStart(2, '0');
+  refs.secondsEl.textContent = seconds.toString().padStart(2, '0');
+}
